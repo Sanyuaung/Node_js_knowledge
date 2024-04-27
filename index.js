1ae1c412dc583029afd18169e3c7b9fb892a9fdc
Sanyuaung <sanyuaung.ygn.mm@gmail.com>
2024-04-27 19:55:03 +0630

    what is async and what is non blocking

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // console.log("Hello SYA");
 const fs = require('fs');
+
 console.log('start program');
-const node = fs.readFileSync('node.txt');
-console.log(node.toString());
+fs.readFile('node.txt', (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+})
+// const node = fs.readFileSync('node.txt');
+// console.log(node.toString());
 console.log('end program');
