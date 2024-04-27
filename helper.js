255fb2a72c856e4b2598c9a17407667108a790e5
Sanyuaung <sanyuaung.ygn.mm@gmail.com>
2024-04-27 20:03:13 +0630

    first module

---
+++ helper.js
@@ -0,0 +1,5 @@
+exports.sum= (num1,num2)=>{
+    return num1+num2;
+};
+
+exports.isLikeBlack=true;
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // console.log("Hello SYA");
-const fs = require('fs');
+// const fs = require('fs');
 
-console.log('start program');
-fs.readFile('node.txt', (err, data) => {
-    if (err) throw err;
-    console.log(data.toString());
-})
+// console.log('start program');
+// fs.readFile('node.txt', (err, data) => {
+//     if (err) throw err;
+//     console.log(data.toString());
+// })
 // const node = fs.readFileSync('node.txt');
 // console.log(node.toString());
-console.log('end program');
+// console.log('end program');
+
+const h = require('./helper');
+console.log(h.sum(8,9));
+console.log(h.isLikeBlack);
